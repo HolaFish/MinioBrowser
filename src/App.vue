46eb22ad612dfffc9afe981755721a1db3dc303e
Heisenberg <15378172149@163.com>
2021-08-15 10:30:44 +0800

    引入Minio 的 JS SDK

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-system-bar app>
+      <v-icon>mdi-message</v-icon>
+      <span>10 unread messages</span>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-minus</v-icon>
+      <v-icon>mdi-checkbox-blank-outline</v-icon>
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar>
+    <v-navigation-drawer app>
+      <buckets-list></buckets-list>
+    </v-navigation-drawer>
+    <v-main>
+      abc
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BucketsList from './components/BucketsList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    BucketsList,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/BucketsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+import globleConfig from '../../globleConfig.json'
+export default {
+    data(){
+        return {
+            documentTree:[]
+        }
+    },
+    created(){
+        var Minio = require('minio')
+var minioClient = new Minio.Client(globleConfig.minio);
+        minioClient.listBuckets((error, buckets) => {
+            if (error){
+                console.log(error);
+            }
+            console.log(buckets);
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
