54e776db236566e70ebe6cf5d9cb841b860e10bd
Heisenberg <15378172149@163.com>
2021-08-16 21:51:43 +0800

    获取bucket

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <v-system-bar app>
-      <v-icon>mdi-message</v-icon>
-      <span>10 unread messages</span>
+      <v-icon color="blue">mdi-web-box</v-icon>
+      <span>Minio Browser</span>
       <v-spacer></v-spacer>
       <v-icon>mdi-minus</v-icon>
       <v-icon>mdi-checkbox-blank-outline</v-icon>
--- src/components/BucketsList.vue
+++ src/components/BucketsList.vue
@@ -1,24 +1,63 @@
 <template>
-  <div></div>
+  <v-treeview
+    v-model="tree"
+    :items="documentTree"
+    :open="open"
+    activatable
+    item-key="name"
+    open-on-click
+  >
+    <template v-slot:prepend="{ item, open }">
+      <v-icon v-if="!item.file">
+        {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+      </v-icon>
+      <v-icon v-else>
+        {{ files[item.file] }}
+      </v-icon>
+    </template>
+  </v-treeview>
 </template>
 
 <script>
-
 import globleConfig from '../../globleConfig.json'
 export default {
     data(){
         return {
-            documentTree:[]
+            documentTree:[],
+            tree:[],
+            open:[],
+            files: {
+                html: 'mdi-language-html5',
+                js: 'mdi-nodejs',
+                json: 'mdi-code-json',
+                md: 'mdi-language-markdown',
+                pdf: 'mdi-file-pdf',
+                png: 'mdi-file-image',
+                txt: 'mdi-file-document-outline',
+                xls: 'mdi-file-excel',
+            },
         }
     },
     created(){
         var Minio = require('minio')
-var minioClient = new Minio.Client(globleConfig.minio);
+        var minioClient = new Minio.Client(globleConfig.minio);
+        let _this = this;
         minioClient.listBuckets((error, buckets) => {
             if (error){
-                console.log(error);
+                _this.$notify({
+                    type:'error',
+                    message:'连接Minio服务器失败！'
+                })
             }
-            console.log(buckets);
+            buckets.forEach(bucket => {
+                bucket.children = [];
+                let stream = minioClient.listObjects(bucket.name,'',false)
+                stream.on('data',(obj) => {
+                    console.log(obj);
+                });
+                stream.on('error',error => console.log(error));
+                _this.documentTree.push(bucket)
+            });
         });
     }
 }
